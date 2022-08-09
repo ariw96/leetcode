1126812ee4be548fb2a1778a792b37a72a02effe
ariw96 <ariw96@gmail.com>
2022-08-09 07:07:41 +0300

    9/8

--- solved-easy/longestCommon.js
+++ solved-easy/longestCommon.js
@@ -21,5 +21,24 @@ var longestCommonPrefix = function (strs) {
 	}
 	return prefix;
 };
-
-console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+var longestCommonPrefix2 = function (strs) {
+	while (strs.length > 1) {
+		if (strs[0].length > strs[strs.length - 1].length) {
+			let temp = strs[0];
+			strs[0] = strs[strs.length - 1];
+			strs[strs.length - 1] = temp;
+		}
+		for (let i = 0; i < strs[0].length; i++) {
+			if (strs[0][i] !== strs[strs.length - 1][i]) {
+				strs[0] = strs[0].substring(0, i);
+				i = strs[0].length;
+			}
+		}
+		strs.pop();
+	}
+	if (strs[0]) {
+		return strs[0];
+	}
+	return "";
+};
+console.log(longestCommonPrefix2(["flower", "flow", "flight"]));
