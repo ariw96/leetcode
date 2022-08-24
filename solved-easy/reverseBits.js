e919b086bbdf27649644288a1f849b80fe1beaae
ariw96 <ariw96@gmail.com>
2022-08-24 06:12:05 +0300

    init

--- solved-easy/reverseBits.js
+++ solved-easy/reverseBits.js
@@ -1,1 +1,7 @@
-var reverseBits = function (n) {};
+var reverseBits = function (n) {
+	let str = n.toString();
+	let reverseStr = str.split("").reverse().join("");
+	let result = parseInt(reverseStr, 2);
+	return result;
+};
+// https://leetcode.com/problems/reverse-bits/
--- solved-easy/sortedArr.js
+++ solved-easy/sortedArr.js
@@ -1,2 +1,16 @@
-var sortedArrayToBST = function (nums) {};
+class Node {
+	constructor(d) {
+		this.data = d;
+		this.left = null;
+		this.right = null;
+	}
+}
+var sortedArrayToBST = function (nums) {
+	if (nums === null || !nums.length) return null;
+	let mid = Math.floor(nums.length / 2);
+	const node = new Node(nums[mid]);
+	node.left = sortedArrayToBST(nums.slice(0, mid));
+	node.right = sortedArrayToBST(nums.slice(mid + 1, nums.length));
+	return node;
+};
 // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
