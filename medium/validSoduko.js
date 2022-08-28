e458219d4177faf7377a2921cad7882b5acdda3e
ariw96 <ariw96@gmail.com>
2022-08-28 12:27:02 +0300

    init

---
+++ medium/validSoduko.js
@@ -0,0 +1,6 @@
+var isValidSudoku = function (board) {
+	let map = {};
+	for (let i = 0; i < 9; i++) {
+		for (let j = 0; j < 9; j++) {}
+	}
+};
--- solved-easy/fizzBazz.js
+++ solved-easy/fizzBazz.js
@@ -1,21 +1,15 @@
-function arrange(sentence) {
+function fizzBuzz(n) {
 	// Write your code here
-
-	let arrangeSentence = sentence
-		.toLowerCase()
-		.split(" ")
-		.sort((a, b) => {
-			if (a.length > b.length) {
-				return 1;
-			} else if (a.length < b.length) {
-				return -1;
-			} else {
-				return 0;
-			}
-		})
-		.join(" ");
-	arrangeSentence.replace(".", "");
-	arrangeSentence =
-		arrangeSentence.charAt(0).toUpperCase() + arrangeSentence.slice(1) + ".";
-	return arrangeSentence;
+	for (let i = 1; i <= n; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log("FizzBuzz");
+		} else if (i % 3 === 0) {
+			console.log("Fizz");
+		} else if (i % 5 === 0) {
+			console.log("Buzz");
+		} else {
+			console.log(i);
+		}
+	}
 }
+console.log(fizzBuzz(15));
