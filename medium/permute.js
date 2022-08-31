730c5b09423ea66ec1134dca6333365afdccb69e
ariw96 <ariw96@gmail.com>
2022-08-31 07:12:18 +0300

    daily

--- medium/permute.js
+++ medium/permute.js
@@ -1,4 +1,17 @@
 var permute = function (nums) {
 	let result = [];
+	let length = 0
+	if (length === 0) return [];
+	if (length === 1) return [nums];
+	for(let i = 0; i< length; i++){
+		const curNum = nums[i]
+		const remainNums = nums.slice(0,i).concat(i+1)
+		const remainNumsPremute = permute(remainNums)
+		for(let j = 0; j < remainNumsPremute.length; j++ ){
+			result.push([curNum,...])
+		}
+	}
+	
+	return result
 };
 // https://leetcode.com/problems/permutations
