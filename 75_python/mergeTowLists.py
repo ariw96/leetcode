c6fa4fe3811585cd790e1a7c8c9b2b9bf6d4d79d
ariw96 <ariw96@gmail.com>
2022-09-09 07:45:57 +0300

    daily

--- 75_python/mergeTowLists.py
+++ 75_python/mergeTowLists.py
@@ -1,14 +1,19 @@
 # https://leetcode.com/problems/merge-two-sorted-lists/?envType=study-plan&id=level-1
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        cur = dummy = listNode()
+        cur = dummy = ListNode()
         while list1 and list2:
             if list1.val < list2.val: 
                 cur.next = list1
                 list1 = list1.next
-                cur = list1
             else:
                 cur.next = list2
                 list2 = list2.next
-                cur = list2
-                
+            cur = cur.next    
+        if list1:
+            cur.next = list1
+        if list2:
+            cur.next = list2
+
+        return dummy.next        
+
